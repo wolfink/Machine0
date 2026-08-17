404817c36593abcafe91de5e37220022fd8e3edf
Max Sherr <maximiliansherr@gmail.com>
2026-08-17 13:50:37 -0400

    Add FM to oscillators' output

--- src/synth/Oscillators.cpp
+++ src/synth/Oscillators.cpp
@@ -24,23 +24,32 @@ void Oscillators::Prepare(int note)
 double Oscillators::OutputNext(double sr, double& a1, double& a2, double& a3, double& a4)
 {
   auto TWOPI = juce::MathConstants<double>::twoPi;
+
+  auto o1_out = sin(a1);
+  auto o2_out = sin(a2
+                    + o1_out * _osc1_fm2);
+  auto o3_out = sin(a3
+                    + o1_out * _osc1_fm3
+                    + o2_out * _osc2_fm3);
+  auto o4_out = sin(a4
+                    + o1_out * _osc1_fm4
+                    + o2_out * _osc2_fm4
+                    + o3_out * _osc3_fm4);
+
   auto o1_delta = TWOPI * _osc1_freq / sr;
   auto o2_delta = TWOPI * _osc2_freq / sr;
   auto o3_delta = TWOPI * _osc3_freq / sr;
   auto o4_delta = TWOPI * _osc4_freq / sr;
-  auto o1_out = sin(a1 += o1_delta);
-  auto o2_out = sin(a2 += o2_delta);
-  auto o3_out = sin(a3 += o3_delta);
-  auto o4_out = sin(a4 += o4_delta);
-  a1 = (a1 > TWOPI) ? a1 - TWOPI : a1;
-  a2 = (a2 > TWOPI) ? a2 - TWOPI : a2;
-  a3 = (a3 > TWOPI) ? a3 - TWOPI : a3;
-  a4 = (a4 > TWOPI) ? a4 - TWOPI : a4;
+
+  a1 = ((a1 += o1_delta) > TWOPI) ? a1 - TWOPI : a1;
+  a2 = ((a2 += o2_delta) > TWOPI) ? a2 - TWOPI : a2;
+  a3 = ((a3 += o3_delta) > TWOPI) ? a3 - TWOPI : a3;
+  a4 = ((a4 += o4_delta) > TWOPI) ? a4 - TWOPI : a4;
   return 
-  	  o1_out * _osc1_gain
-  	+ o2_out * _osc2_gain
-  	+ o3_out * _osc3_gain
-  	+ o4_out * _osc4_gain;
+      o1_out * _osc1_gain
+    + o2_out * _osc2_gain
+    + o3_out * _osc3_gain
+    + o4_out * _osc4_gain;
 }
 
 double Oscillators::Get_osc_to_freq(int note, int index)
